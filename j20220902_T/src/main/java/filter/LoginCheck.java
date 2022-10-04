bb56c78ab89646ed173e96720c94faefeb2bfa25
seanj <seanj@DESKTOP-E3NLJQV>
2022-10-04 21:32:47 +0900

    예약,마이페이지시filter[제환]

--- j20220902_T/src/main/java/filter/LoginCheck.java
+++ j20220902_T/src/main/java/filter/LoginCheck.java
@@ -1,9 +1,12 @@
 package filter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -12,26 +15,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter("/Jehwan/*")
-public class LoginCheck implements Filter {
+	
 
+public class LoginCheck implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		
-		
+		System.out.println("LoginCheck doFilter ...");
 		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
 		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
 		// Session 도출 방법
 		HttpSession session = httpServletRequest.getSession();
-		System.out.println("LoginCheck doFilter ...");
+		//String path = httpServletRequest.getServletPath();
+	    String context = httpServletRequest.getContextPath();
+	    System.out.println(context);
 		if(session == null || session.equals("")) {
-			httpServletResponse.sendRedirect("../GunHee/loginForm.jsp");
+			httpServletResponse.sendRedirect("/j20220902_T/GunHee/loginForm.jsp");
+			return;
 		}
 		String id = (String)session.getAttribute("id");
 		if(id == null || id.equals("")) {
-			httpServletResponse.sendRedirect("../GunHee/loginForm.jsp");
+			httpServletResponse.sendRedirect("/j20220902_T/GunHee/loginForm.jsp");
+			return;
 		}
+
 		chain.doFilter(request, response);
 	}
 
